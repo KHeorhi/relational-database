21334d2391bba69ada2cd70202d94ad890390cd2
KHeorhi <88582476+KHeorhi@users.noreply.github.com>
2022-04-02 16:45:53 +0300

    Update universe.sql
    В таблицы stat, planet, moon добавлены внешние ключи.

--- celestial-bodies-database/universe.sql
+++ celestial-bodies-database/universe.sql
@@ -271,6 +271,30 @@ ALTER TABLE ONLY public.star
 
 
 --
+-- Name: moon moon_planet_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.moon
+    ADD CONSTRAINT moon_planet_id_fkey FOREIGN KEY (planet_id) REFERENCES public.planet(planet_id);
+
+
+--
+-- Name: planet planet_star_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.planet
+    ADD CONSTRAINT planet_star_id_fkey FOREIGN KEY (star_id) REFERENCES public.star(star_id);
+
+
+--
+-- Name: star star_galaxy_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.star
+    ADD CONSTRAINT star_galaxy_id_fkey FOREIGN KEY (galaxy_id) REFERENCES public.galaxy(galaxy_id);
+
+
+--
 -- PostgreSQL database dump complete
 --
 
