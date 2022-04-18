7e6a2c4161317e80e645e031e7c451680e1358d9
Heorhi Kirpichenko <88582476+KHeorhi@users.noreply.github.com>
2022-04-18 23:34:13 +0300

    Update insert_data.sh
    add capability to add data from games.csv to games data

--- world_cup_database/insert_data.sh
+++ world_cup_database/insert_data.sh
@@ -36,6 +36,9 @@ do
       echo $($PSQL "INSERT INTO teams(name)VALUES('$OPPONENT')")
     fi
   fi
-
+  #get winner_id and opponent_id
+  WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+  OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+  echo $($PSQL "INSERT INTO games(year,round,winner_id, opponent_id,winner_goals, opponent_goals)VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
   
 done
