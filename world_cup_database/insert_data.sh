b1e7c259fa5da355836a3bb2e4e609d00f86f04f
Heorhi Kirpichenko <88582476+KHeorhi@users.noreply.github.com>
2022-04-18 22:05:52 +0300

    Update insert_data.sh
    add new  capability to add data into teams table from games.csv

--- world_cup_database/insert_data.sh
+++ world_cup_database/insert_data.sh
@@ -8,3 +8,34 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+echo $($PSQL "TRUNCATE games, teams")
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $WINNER != "winner" ]]
+  then
+  # not in team_id
+  NAME=$($PSQL "SELECT name FROM teams WHERE name='$WINNER'")
+    
+    if [[ -z $NAME ]]
+    then
+    #insert team_id
+      echo $($PSQL "INSERT INTO teams(name)VALUES('$WINNER')")
+    else [[ $WINNER != $NAME ]]
+      echo $($PSQL "INSERT INTO teams(name)VALUES('$WINNER')")
+    fi
+  fi
+
+  if [[ $OPPONENT != "opponent" ]]
+  then
+   # not in name
+   NOT_IN_NAME=$($PSQL "SELECT name FROM teams WHERE name='$OPPONENT'")
+
+    if [[ -z $NOT_IN_NAME ]]
+    then
+      #insert opponent
+      echo $($PSQL "INSERT INTO teams(name)VALUES('$OPPONENT')")
+    fi
+  fi
+
+  
+done
